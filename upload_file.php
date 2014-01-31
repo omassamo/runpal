146b5973d73e1281ab9f9a2907cbb23a78176860
Matias Bruhn <matias.bruhn@bacondeczar.comgit config --global user.name Matias>
2014-01-31 08:16:13 +0100

    added tracking to upload_file.php

--- upload_file.php
+++ upload_file.php
@@ -74,3 +74,13 @@ for($i=0; $i<count($_FILES['upload']['name']); $i++) {
       echo "Error";
   }
 ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47568451-1', 'runpal.org');
+  ga('send', 'pageview');
+
+</script>
